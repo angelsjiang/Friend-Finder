5850e7d9ea0393b01042ee99fdd7cf1a6a3b86b4
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-11-07 19:48:19 -0800

    modal popped up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,11 +9,58 @@ var friendArray = require('../data/friends');
 
 module.exports = function(app) {
     
+    // API GET request
     app.get('/api/friends', function(req, res) {
         res.json(friendArray);
     });
 
+    // API POST request
     app.post('/api/friends', function(req, res) {
-        friendArray.push(req.body);
+        
+        console.log("From apiroutes server: " + req.body.photo);
+        if(req.body.photo !== "") {
+
+            // add new friend
+            friendArray.push(req.body);
+
+            // function that goes through the req & perform calculations
+            var usrScore;
+            for(var i = 0; i < req.body.scores.length; i++) {
+                usrScore += req.body.scores[i];
+            };
+
+            // need a function that goes through the friend array
+            var closestScore = 0;
+            // max diff is 50 - 10 = 40
+            var closestDiff = 90; 
+            // who's my closest friend?
+            var closestFriend = {};
+            for(var i = 0; i < friendArray.length; i++) {
+
+                // go through each friend and calculate scores
+                for(var j = 0; j < friendArray[i].scores.length; j++) {
+                    closestScore += friendArray[i].scores[j]; 
+                };
+
+                // after getting the closestScore, evaluate with the usrScore
+                var diff = Math.abs(usrScore - closestScore);
+                if(diff < closestDiff) {
+                    closestDiff = diff;
+                    closestFriend = friendArray[i];
+                };
+                
+            };
+
+            // return true statement
+            res.json(closestFriend);
+
+            // HOWEVER, instead of returning "true", need to parse 
+            // the information as name and photo
+        }
+        else {
+            // HOWEVER, instead of returning "false", should return "please
+            // complete required fields"
+            res.json(false);
+        }
     });
-}
+};
